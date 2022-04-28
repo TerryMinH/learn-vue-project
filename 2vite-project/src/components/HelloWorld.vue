5d81d8f0cbf536c577ebd66efa55f61d13ba2c6a
TerryMin <15896938825@163.com>
2022-04-28 15:53:05 +0800

    code format

--- 2vite-project/src/components/HelloWorld.vue
+++ 2vite-project/src/components/HelloWorld.vue
@@ -2,24 +2,16 @@
  * @Author: TerryMin
  * @Date: 2022-04-26 10:00:09
  * @LastEditors: TerryMin
- * @LastEditTime: 2022-04-26 14:52:00
+ * @LastEditTime: 2022-04-28 15:52:06
  * @Description: file not
 -->
 <template>
-  <h1>{{ msg }}</h1>
-  <button
-    type="button"
-    @click="count++"
-  >count is: {{ count }}</button>
+	<h1>{{ msg }}</h1>
+	<button type="button" @click="count++">count is: {{ count }}</button>
 
-  <button
-    type="button"
-    @click="changeBtn"
-  >{{state.time}}</button>
-  <view @click="handleBtn" ref="box">
-    点击新增
-  </view>
-  <!-- <p>
+	<button type="button" @click="changeBtn">{{ state.time }}</button>
+	<view @click="handleBtn" ref="box"> 点击新增 </view>
+	<!-- <p>
     Recommended IDE setup:
     <a
       href="https://code.visualstudio.com/"
@@ -45,36 +37,33 @@
       target="_blank"
     >Vue 3 Documentation</a>
   </p> -->
-
 </template>
 
 <script setup>
-import { ref, reactive, onMounted, defineEmits } from 'vue'
+import { ref, reactive, onMounted, defineEmits } from "vue";
 
 defineProps({
-  msg: String
-})
+	msg: String,
+});
 
 const count = ref(0);
-const state = reactive({ time: '时光' })
-const emits=defineEmits(['onExport'])
-const boxRef=ref(null)
+const state = reactive({ time: "时光" });
+const emits = defineEmits(["onExport"]);
+const boxRef = ref(null);
 
-const handleBtn=()=>{
-  emits('onExport','emits---')
-}
+const handleBtn = () => {
+	emits("onExport", "emits---");
+};
 
 // 生命周期钩子
 onMounted(() => {
-  console.log(`The initial count is ${count.value}.`)
-  console.log(boxRef)
-})
+	console.log(`The initial count is ${count.value}.`);
+	console.log(boxRef);
+});
 
 const changeBtn = () => {
-  state.time = '时光, 你好!'
-}
-
+	state.time = "时光, 你好!";
+};
 </script>
 
-<style lang="" scoped>
-</style>
+<style lang="" scoped></style>
--- typescript.ts
+++ typescript.ts
@@ -2,30 +2,30 @@
  * @Author: TerryMin
  * @Date: 2021-11-12 13:56:46
  * @LastEditors: TerryMin
- * @LastEditTime: 2021-11-12 13:59:45
+ * @LastEditTime: 2022-04-28 15:49:53
  * @Description: file not
  */
 
 interface foo {
-  x: number
+	x: number;
 }
 
 interface bar {
-  b: number
+	b: number;
 }
 
 type intersection = foo & bar;
 
 const result: intersection = {
-  x: 10,
-  b: 3
-}
+	x: 10,
+	b: 3,
+};
 
 // 联合类型
 type arg = string | number | boolean;
 
 const foo = (arg: arg): any => {
-  console.log(arg)
-}
+	console.log(arg);
+};
 
 foo(1);
