a6b4d6291fa04b37304775e785f20ca950359ff3
huangminguang <17621481287@163.com>
2023-04-10 09:54:46 +0800

    feat: vite

--- vue3-project/vite.config.js
+++ vue3-project/vite.config.js
@@ -2,7 +2,7 @@
  * @Author: TerryMin
  * @Date: 2021-12-11 15:17:16
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-03-02 11:11:11
+ * @LastEditTime: 2023-04-07 11:09:11
  * @Description: file not
  */
 import { defineConfig } from 'vite'
@@ -18,4 +18,7 @@ export default defineConfig({
     },
     extensions: ['.mjs', '.js', '.ts', '.jsx', '.tsx', '.json']
   },
+  server:{
+    host:'0.0.0.0' // 指定服务器应该监听哪个 IP 地址。 如果将此设置为 0.0.0.0 将监听所有地址，包括局域网和公网地址
+  }
 })
