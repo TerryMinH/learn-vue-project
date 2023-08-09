78a1c617e6307232299d449e413729cbc571bb71
huangminguang <17621481287@163.com>
2023-08-09 11:43:26 +0800

    feat: video

--- 3d-demo/src/App.vue
+++ 3d-demo/src/App.vue
@@ -2,40 +2,27 @@
  * @Author: TerryMin
  * @Date: 2022-10-12 07:26:39
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-08-09 11:02:42
+ * @LastEditTime: 2023-08-09 11:40:29
  * @Description: file not
 -->
 <template>
-  <div id="app">
+  <div>
     <TrackFace />
   </div>
 </template>
 
 <script>
-import TrackFace from "./components/TrackFace.vue";
-import dataMixin from "./mixins/data";
+import TrackFace from "@/components/TrackFace.vue";
 export default {
   name: "App",
   components: {
     TrackFace,
   },
   data() {
-    return {
-      isDisplay: true,
-    };
+    return {};
   },
-  mixins: [dataMixin],
   mounted() {},
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style></style>
