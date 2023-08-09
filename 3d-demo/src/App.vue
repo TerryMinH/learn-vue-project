8f9f8264e52aaa03bc843520e2a20f573a401099
huangminguang <17621481287@163.com>
2023-08-09 11:04:43 +0800

    feat: camer

--- 3d-demo/src/App.vue
+++ 3d-demo/src/App.vue
@@ -2,7 +2,7 @@
  * @Author: TerryMin
  * @Date: 2022-10-12 07:26:39
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-08-08 20:43:09
+ * @LastEditTime: 2023-08-09 11:02:42
  * @Description: file not
 -->
 <template>
@@ -37,12 +37,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-}
-canvas {
-  width: 375px;
-  height: 400px;
-  /* border: 1px solid #666; */
-  position: absolute;
-  top: 60px;
 }
 </style>
--- 3d-demo/src/components/TrackFace.vue
+++ 3d-demo/src/components/TrackFace.vue
@@ -2,7 +2,7 @@
  * @Author: TerryMin
  * @Date: 2022-10-12 07:26:39
  * @LastEditors: TerryMin
- * @LastEditTime: 2023-08-08 20:56:27
+ * @LastEditTime: 2023-08-09 11:03:43
  * @Description: file not
 -->
 <template>
@@ -23,9 +23,9 @@
 </template>
 
 <script>
+import * as tracking from "tracking";
 import dat from "dat.gui";
-import tracking from "tracking";
-import "../assets/lib/data/face-min.js";
+import "@/assets/lib/data/face-min.js";
 // import "../assets/lib/data/stats-min.js";
 
 export default {
@@ -46,45 +46,44 @@ export default {
 
   methods: {
     init() {
-      window.onload = function () {
-        // var video = document.getElementById("video");
-        var canvas = document.getElementById("canvas");
-        var context = canvas.getContext("2d");
+      // console.log(window.tracking);
+      var video = document.getElementById("video");
+      var canvas = document.getElementById("canvas");
+      var context = canvas.getContext("2d");
 
-        var tracker = new tracking.ObjectTracker("face"); // 设置追踪类型
-        tracker.setInitialScale(4);
-        tracker.setStepSize(2);
-        tracker.setEdgesDensity(0.1);
+      var tracker = new window.tracking.ObjectTracker("face"); // 设置追踪类型
+      tracker.setInitialScale(4);
+      tracker.setStepSize(2);
+      tracker.setEdgesDensity(0.1);
 
-        tracking.track("#video", tracker, { camera: true });
+      window.tracking.track("#video", tracker, { camera: true });
 
-        tracker.on("track", function (event) {
-          // console.log(event);
-          context.clearRect(0, 0, canvas.width, canvas.height);
+      tracker.on("track", function (event) {
+        console.log(event);
+        context.clearRect(0, 0, canvas.width, canvas.height);
 
-          event.data.forEach(function (rect) {
-            context.strokeStyle = "#a64ceb";
-            context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-            context.font = "11px Helvetica";
-            context.fillStyle = "#fff";
-            context.fillText(
-              "x: " + rect.x + "px",
-              rect.x + rect.width + 5,
-              rect.y + 11
-            );
-            context.fillText(
-              "y: " + rect.y + "px",
-              rect.x + rect.width + 5,
-              rect.y + 22
-            );
-          });
+        event.data.forEach(function (rect) {
+          context.strokeStyle = "#a64ceb";
+          context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          context.font = "11px Helvetica";
+          context.fillStyle = "#fff";
+          context.fillText(
+            "x: " + rect.x + "px",
+            rect.x + rect.width + 5,
+            rect.y + 11
+          );
+          context.fillText(
+            "y: " + rect.y + "px",
+            rect.x + rect.width + 5,
+            rect.y + 22
+          );
         });
+      });
 
-        var gui = new dat.GUI();
-        gui.add(tracker, "edgesDensity", 0.1, 0.5).step(0.01);
-        gui.add(tracker, "initialScale", 1.0, 10.0).step(0.1);
-        gui.add(tracker, "stepSize", 1, 5).step(0.1);
-      };
+      // var gui = new dat.GUI();
+      // gui.add(tracker, "edgesDensity", 0.1, 0.5).step(0.01);
+      // gui.add(tracker, "initialScale", 1.0, 10.0).step(0.1);
+      // gui.add(tracker, "stepSize", 1, 5).step(0.1);
     },
   },
 };
@@ -93,8 +92,9 @@ export default {
 <style scoped>
 video,
 canvas {
-  margin-left: 230px;
-  margin-top: 120px;
   position: absolute;
+  top: 0;
+  left: 0;
+  border: #000000 5px solid;
 }
 </style>
