7e121295516cb115203db202e83a729ff925f156
huangminguang <17621481287@163.com>
2024-05-22 19:14:19 +0800

    md5 加解密

--- index.js
+++ index.js
@@ -2,12 +2,12 @@
  * @Author: TerryMin
  * @Date: 2023-09-25 20:22:44
  * @LastEditors: TerryMin
- * @LastEditTime: 2024-05-12 15:33:26
+ * @LastEditTime: 2024-05-20 11:07:24
  * @Description: file not
  */
 const months = ["Jan", "Mar", "Apr", "May"];
 
 // 在索引 1 处添加一个元素
-const months2 = months.toSpliced(1, 0, "Feb");
-console.log(months2); 
-console.log(months); 
+// const months2 = months.toSpliced(1, 0, "Feb");
+const b = new ArrayBuffer(8);
+console.log(b);
